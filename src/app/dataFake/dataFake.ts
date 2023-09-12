bf3487822ba049c46ec2765d9ed47ffd66275900
Saulo Andrade <sauloandradegames@gmail.com>
2023-09-12 19:20:58 -0300

    first commit

---
+++ src/app/dataFake/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+    {
+        "id":"1",
+        "photo":"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fdevelopers.elementor.com%2Fdocs%2Fassets%2Fimg%2Felementor-placeholder-image.png&f=1&nofb=1&ipt=02dc0aa12979af9e6b4a26ebc11f84b1d2daddf0c8b64c276be11ba6334eb7fb&ipo=images",
+        "title":"Noticia Titulo 01",
+        "description":"avestruz"
+    },
+    {
+        "id":"2",
+        "photo":"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fdevelopers.elementor.com%2Fdocs%2Fassets%2Fimg%2Felementor-placeholder-image.png&f=1&nofb=1&ipt=02dc0aa12979af9e6b4a26ebc11f84b1d2daddf0c8b64c276be11ba6334eb7fb&ipo=images",
+        "title":"Noticia Titulo 02",
+        "description":"pepino"
+    },
+    {
+        "id":"3",
+        "photo":"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fdevelopers.elementor.com%2Fdocs%2Fassets%2Fimg%2Felementor-placeholder-image.png&f=1&nofb=1&ipt=02dc0aa12979af9e6b4a26ebc11f84b1d2daddf0c8b64c276be11ba6334eb7fb&ipo=images",
+        "title":"Noticia Titulo 03",
+        "description":"whatever"
+    },
+    {
+        "id":"4",
+        "photo":"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fdevelopers.elementor.com%2Fdocs%2Fassets%2Fimg%2Felementor-placeholder-image.png&f=1&nofb=1&ipt=02dc0aa12979af9e6b4a26ebc11f84b1d2daddf0c8b64c276be11ba6334eb7fb&ipo=images",
+        "title":"Noticia Titulo 04",
+        "description":"lul"
+    },
+]
